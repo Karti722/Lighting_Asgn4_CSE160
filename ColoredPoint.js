de6bafc5341c9ad587e67a2fbbc5cce3e89dd030
Kartikeya <kartikeyaku@gmail.com>
2025-01-21 19:32:44 -0800

    Finished part 4

--- ColoredPoint.js
+++ ColoredPoint.js
@@ -33,20 +33,19 @@ function setUpWebGL() {
   }
 }  
 
+
 function connectVariablesToGLSL() {
  // Initialize shaders
  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
     console.log('Failed to intialize shaders.');
     return;
   }
-
   // // Get the storage location of a_Position
   a_Position = gl.getAttribLocation(gl.program, 'a_Position');
   if (a_Position < 0) {
     console.log('Failed to get the storage location of a_Position');
     return;
   }
-
   // Get the storage location of u_FragColor
   u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
   if (!u_FragColor) {
@@ -56,11 +55,29 @@ function connectVariablesToGLSL() {
 }
 
 
+let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
+
+
+function addActionsForHtmlUI () {
+
+    // Button events (Shape type)
+    document.getElementById("green").onclick = function() { g_selectedColor = [0.0, 1.0, 0.0, 1.0] };
+    document.getElementById("red").onclick = function() { g_selectedColor = [1.0, 0.0, 0.0, 1.0] };
+
+    //Slider events
+    document.getElementById('redSlide').addEventListener('mouseup', function() { g_selectedColor[0] = this.value/100; });
+    document.getElementById('blueSlide').addEventListener('mouseup', function() { g_selectedColor[1] = this.value/100; });
+    document.getElementById('greenSlide').addEventListener('mouseup', function() { g_selectedColor[2] = this.value/100; });
+}
+
 function main() {
+    // Sets up WebGL canvas and context
   setUpWebGL();
+
   connectVariablesToGLSL();
 
- 
+//   Handles the clicking of the red and green buttons
+  addActionsForHtmlUI();
 
   // Register function (event handler) to be called on a mouse press
   canvas.onmousedown = click;
@@ -77,30 +94,29 @@ var g_colors = [];  // The array to store the color of a point
 function click(ev) {
 
     // Extract the event click and turn it into WebGL coordinates
- [x, y] = convertCoordinatesEventToGL(ev);
+[x, y] = convertCoordinatesEventToGL(ev);
   // Store the coordinates to g_points array
-  g_points.push([x, y]);
+g_points.push([x, y]);
   // Store the coordinates to g_points array
-  console.log(g_colors[g_colors.length-1]);
-  if (x >= 0.0 && y >= 0.0) {      // First quadrant
-    g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
-  } else if (x < 0.0 && y < 0.0) { // Third quadrant
-    g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
-  } else {                         // Others
-    g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
-  }
 
-  renderAllShapes();
+g_colors.push([g_selectedColor[0], g_selectedColor[1], g_selectedColor[2], g_selectedColor[3]]);
+//   if (x >= 0.0 && y >= 0.0) {      // First quadrant
+//     g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
+//   } else if (x < 0.0 && y < 0.0) { // Third quadrant
+//     g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
+//   } else {                         // Others
+//     g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
+//   }
+
+renderAllShapes();
 }
 
 function convertCoordinatesEventToGL (ev) {
     var x = ev.clientX; // x coordinate of a mouse pointer
     var y = ev.clientY; // y coordinate of a mouse pointer
     var rect = ev.target.getBoundingClientRect();
-  
     x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
     y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
-
     return [x, y];
 }
 

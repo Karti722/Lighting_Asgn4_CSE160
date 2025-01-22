6282adf9008f60e7dc0441d8eb3579f3495c58d9
Kartikeya <kartikeyaku@gmail.com>
2025-01-21 22:03:02 -0800

    Finished parts 6 and 7

--- ColoredPoint.js
+++ ColoredPoint.js
@@ -69,6 +69,7 @@ function addActionsForHtmlUI () {
     // Button events (Shape type)
     document.getElementById("green").onclick = function() { g_selectedColor = [0.0, 1.0, 0.0, 1.0] };
     document.getElementById("red").onclick = function() { g_selectedColor = [1.0, 0.0, 0.0, 1.0] };
+    document.getElementById("clearButton").onclick = function() { g_shapesList = []; renderAllShapes() };
 
     //Slider events
     document.getElementById('redSlide').addEventListener('mouseup', function() { g_selectedColor[0] = this.value/100; });
@@ -98,28 +99,38 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-var g_points = [];  // The array for the position of a mouse press
-var g_colors = [];  // The array to store the color of a point
-var g_sizes = [];   // The array to store the sizes of a point
+
+
+var g_shapesList = [];
+// var g_points = [];  // The array for the position of a mouse press
+// var g_colors = [];  // The array to store the color of a point
+// var g_sizes = [];   // The array to store the sizes of a point
 
 function click(ev) {
 
     // Extract the event click and turn it into WebGL coordinates
-    [x, y] = convertCoordinatesEventToGL(ev);
-    // Store the coordinates to g_points array
-    g_points.push([x, y]);
+    let [x, y] = convertCoordinatesEventToGL(ev);
     // Store the coordinates to g_points array
 
-    g_colors.push([g_selectedColor[0], g_selectedColor[1], g_selectedColor[2], g_selectedColor[3]]);
-    //   if (x >= 0.0 && y >= 0.0) {      // First quadrant
-    //     g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
-    //   } else if (x < 0.0 && y < 0.0) { // Third quadrant
-    //     g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
-    //   } else {                         // Others
-    //     g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
-    //   }
+    let point = new Point();
+    point.position = [x, y];
+    point.color = g_selectedColor.slice();
+    point.size = g_selected_size;
+    g_shapesList.push(point);
 
-    g_sizes.push(g_selected_size);
+    // g_points.push([x, y]);
+    // // Store the coordinates to g_points array
+
+    // g_colors.push([g_selectedColor[0], g_selectedColor[1], g_selectedColor[2], g_selectedColor[3]]);
+    // //   if (x >= 0.0 && y >= 0.0) {      // First quadrant
+    // //     g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
+    // //   } else if (x < 0.0 && y < 0.0) { // Third quadrant
+    // //     g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
+    // //   } else {                         // Others
+    // //     g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
+    // //   }
+
+    // g_sizes.push(g_selected_size);
 
     renderAllShapes();
 }
@@ -137,19 +148,8 @@ function renderAllShapes () {
       // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  var len = g_points.length;
+  var len = g_shapesList.length;
   for(var i = 0; i < len; i++) {
-    var xy = g_points[i];
-    var rgba = g_colors[i];
-    var size = g_sizes[i];
-
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    // 
-    gl.uniform1f(u_Size, size);
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
+    g_shapesList[i].render();
   }
 }

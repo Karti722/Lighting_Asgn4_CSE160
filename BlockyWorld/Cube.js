eaba686d1d1c34930d4c7e610671b5bab183029c
Kartikeya <kartikeyaku@gmail.com>
2025-02-13 23:19:41 -0800

    Added buggy UV lighting for Asgn3 (sort of?)

--- BlockyWorld/Cube.js
+++ BlockyWorld/Cube.js
@@ -12,43 +12,16 @@ class Cube {
         gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
         gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
         
-        
-        // Define vertices for each face of the cube
-        let vertices = [
-            // Front face
-            0.0, 0.0, 0.0,   1.0, 1.0, 0.0,   1.0, 0.0, 0.0,
-            0.0, 0.0, 0.0,   0.0, 1.0, 0.0,   1.0, 1.0, 0.0,
-            
-            // Back face
-            0.0, 0.0, 1.0,   1.0, 0.0, 1.0,   1.0, 1.0, 1.0,
-            0.0, 0.0, 1.0,   1.0, 1.0, 1.0,   0.0, 1.0, 1.0,
-            
-            // Top face
-            0.0, 1.0, 0.0,   0.0, 1.0, 1.0,   1.0, 1.0, 1.0,
-            0.0, 1.0, 0.0,   1.0, 1.0, 1.0,   1.0, 1.0, 0.0,
-            
-            // Bottom face
-            0.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 1.0,
-            0.0, 0.0, 0.0,   1.0, 0.0, 1.0,   0.0, 0.0, 1.0,
-            
-            // Left face
-            0.0, 0.0, 0.0,   0.0, 1.0, 1.0,   0.0, 1.0, 0.0,
-            0.0, 0.0, 0.0,   0.0, 0.0, 1.0,   0.0, 1.0, 1.0,
-            
-            // Right face
-            1.0, 0.0, 0.0,   1.0, 1.0, 0.0,   1.0, 1.0, 1.0,
-            1.0, 0.0, 0.0,   1.0, 1.0, 1.0,   1.0, 0.0, 1.0
-        ];
+        // Front of cube
+        drawTriangle3DUV([0,0,0, 1,1,0, 1,0,0 ], [0,0, 1,1, 1,0]);
+        drawTriangle3DUV([0,0,0, 0,1,0, 1,1,0], [0,0, 0,1, 1,1]);
+  
+        // Pass the color of a point to u_FragColor variable
+        gl.uniform4f(u_FragColor, rgba[0]*0.9, rgba[1]*0.9, rgba[2]*0.9, rgba[3]);
 
-        // Draw the cube using triangles
-        for (let i = 0; i < vertices.length; i += 9) {
-            drawTriangle3D(vertices.slice(i, i + 9));
-            // This line adds the lighting for each side of the cube
-            if (i % 2 == 1 && i > 18) {
-                gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
-            }
-        }
-
+        // Top of cube
+        drawTriangle3D([0,1,0, 0,1,1, 1,1,1]);
+        drawTriangle3D([0,1,0, 1,1,1, 1,1,0]);
     
 
     }
--- BlockyWorld/Triangle.js
+++ BlockyWorld/Triangle.js
@@ -74,7 +74,50 @@ function drawTriangle(vertices) {
   
     // Enable the assignment to a_Position variable
     gl.enableVertexAttribArray(a_Position);
-  
+   
     gl.drawArrays(gl.TRIANGLES, 0, n);
 
   }
+
+  function drawTriangle3DUV(vertices, uv) {
+    var n = 3; // The number of vertices
+  
+    // Create a buffer object
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+      console.log('Failed to create the buffer object');
+      return -1;
+    }
+  
+    // Bind the buffer object to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // Write data into the buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+    // Assign the buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+  
+    // Enable the assignment to a_Position variable
+    gl.enableVertexAttribArray(a_Position);
+
+    // Create a buffer object for UV coordinates
+    var uvBuffer = gl.createBuffer();
+    if (!uvBuffer) {
+      console.log('Failed to create the buffer object');
+      return -1;
+    }
+  
+    // Bind the buffer object to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    // Write data into the buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uv), gl.DYNAMIC_DRAW);
+
+    // Assign the buffer object to a_UV variable
+    gl.vertexAttribPointer(a_UV, 2, gl.FLOAT, false, 0, 0);
+  
+    // Enable the assignment to a_UV variable
+    gl.enableVertexAttribArray(a_UV);
+  
+    // Draw the triangles
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}

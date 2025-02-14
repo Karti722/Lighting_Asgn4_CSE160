4994f4fd615ebb16868c2fbc32396ae37c53ee8a
Kartikeya <kartikeyaku@gmail.com>
2025-02-13 23:38:48 -0800

    Finished 3.1 of Asgn3

--- BlockyWorld/Cube.js
+++ BlockyWorld/Cube.js
@@ -11,6 +11,20 @@ class Cube {
         var rgba = this.color;
         gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
         gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+
+        // let vertices = [
+            
+            
+
+            
+        //     // Left face
+        //     0.0, 0.0, 0.0,   0.0, 1.0, 1.0,   0.0, 1.0, 0.0,
+        //     0.0, 0.0, 0.0,   0.0, 0.0, 1.0,   0.0, 1.0, 1.0,
+            
+        //     // Right face
+        //     1.0, 0.0, 0.0,   1.0, 1.0, 0.0,   1.0, 1.0, 1.0,
+        //     1.0, 0.0, 0.0,   1.0, 1.0, 1.0,   1.0, 0.0, 1.0
+        // ];
         
         // Front of cube
         drawTriangle3DUV([0,0,0, 1,1,0, 1,0,0 ], [0,0, 1,1, 1,0]);
@@ -22,6 +36,22 @@ class Cube {
         // Top of cube
         drawTriangle3D([0,1,0, 0,1,1, 1,1,1]);
         drawTriangle3D([0,1,0, 1,1,1, 1,1,0]);
+
+        // Back of cube
+        drawTriangle3D([0,0,1, 1,0,1, 1,1,1]);
+        drawTriangle3D([0,0,1, 1,1,1, 0,1,1]);
+
+        // Bottom of cube
+        drawTriangle3D([0, 0, 0, 1, 0, 0, 1, 0, 1]);
+        drawTriangle3D([0, 0, 0, 1, 0, 1, 0, 0, 1]);
+
+        // left of cube
+        drawTriangle3D([0.0, 0.0, 0.0,   0.0, 1.0, 1.0,   0.0, 1.0, 0.0]);
+        drawTriangle3D([0.0, 0.0, 0.0,   0.0, 0.0, 1.0,   0.0, 1.0, 1.0]);
+
+        // right of cube
+        drawTriangle3D([ 1.0, 0.0, 0.0,   1.0, 1.0, 0.0,   1.0, 1.0, 1.0]);
+        drawTriangle3D([1.0, 0.0, 0.0,   1.0, 1.0, 1.0,   1.0, 0.0, 1.0]);
     
 
     }
